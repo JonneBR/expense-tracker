25b6b38cf588acbbe2b02bccb64bd26a58dff426
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-03-16 13:03:44 -0300

    className changed and addtransction component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import { Header } from './components/Header';
 import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionalList } from './components/TransactionalList';
+import AddTransaction from './components/AddTransaction';
 
 function App() {
   return (
@@ -8,6 +11,9 @@ function App() {
       <Header />
       <div className='container'>
         <Balance />
+        <IncomeExpenses />
+        <TransactionalList />
+        <AddTransaction />
       </div>
     </div>
   );
