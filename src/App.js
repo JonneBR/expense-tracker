c1dcc93034d767dcb1e5f238b50a848556b7826e
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-03-16 12:56:05 -0300

    header and balance component and global css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Header />
+      <div className='container'>
+        <Balance />
+      </div>
     </div>
   );
 }
