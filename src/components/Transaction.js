9eb633203dde8366363b7de53dea2d515535b182
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-03-18 10:41:02 -0300

    dinamic border color and sign

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 export const Transaction = ({ transaction }) => {
+  const sign = transaction.amount < 0 ? '-' : '+';
+  console.log(sign);
+
   return (
-    <li className='minus'>
-      {transaction.text} <span>{transaction.amount}</span>
+    <li className={transaction.amount > 0 ? 'plus' : 'minus'}>
+      {transaction.text}
+      <span>
+        {sign}${Math.abs(transaction.amount)}
+      </span>
       <button className='delete-btn'>x</button>
     </li>
   );
