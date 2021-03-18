f11aa200d01b69f780a80d3c15bf727e973940df
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-03-18 15:03:36 -0300

    dinamic calculation of incomes and expenses

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export const IncomeExpenses = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  const IncomeValues = [];
+  const ExpenseValues = [];
+
+  transactions.filter((income) =>
+    income.amount > 0
+      ? IncomeValues.push(income.amount)
+      : ExpenseValues.push(Math.abs(income.amount))
+  );
   return (
     <div className='inc-exp-container'>
       <div>
         <h4>Income</h4>
         <p id='money-plus' className='money plus'>
-          +$0.00
+          +${IncomeValues.reduce((acc, item) => acc + item, 0)}
         </p>
       </div>
       <div>
         <h4>Expense</h4>
         <p id='money-minus' className='money minus'>
-          -$0.00
+          -${ExpenseValues.reduce((acc, item) => acc + item, 0)}
         </p>
       </div>
     </div>
